0c4771cb9bbee1def13b2e5b3fc294d440261dcd
wugren <wugren@gmail.com>
2025-03-31 11:47:23 +0800

    remove unused file

--- run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker run --rm -d -p 8888:80 -p 3424:3424/udp --name vpn-server \
--e VPN_ADMIN_NAME=wugren \
--e VPN_ADMIN_PASSWORD=123456 \
--e VPN_JWT_KEY=dkflseosoidfsjdkflsdsfs \
--e VPN_DATA_DIR=/test_data \
--v /root/work/vpn/test_data:/test_data vpn-server:latest
-
